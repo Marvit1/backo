deeccf4ff98f8cc967f9417e7f9e3f2aef1a9763
Marvit1 <arsenbg2014@gmail.com>
2024-11-15 14:28:15 +0400

    sd

--- back/settings.py
+++ back/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1+x9ie7mansn0ge&$brta83@lms23%s#cg9a%i8pp_yyhf&58l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://backo-aa1b430f6434.herokuapp.com']
+ALLOWED_HOSTS = ['backo-aa1b430f6434.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
